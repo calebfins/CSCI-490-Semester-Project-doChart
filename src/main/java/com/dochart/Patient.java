a8ec67b9d1612b76f69b7810424c99c95643c6dc
Caleb Fins <calebwfins@gmail.com>
2020-09-10 15:04:33 -0400

    init

---
+++ src/main/java/com/dochart/Patient.java
@@ -0,0 +1,26 @@
+package com.dochart;
+
+import lombok.Data;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.SEQUENCE)
+    @SequenceGenerator(name="Patient_SEQ", allocationSize= 10000)
+    private Integer id;
+    private int age;
+    private int height;
+    private int weight;
+    private String fname;
+    private String lname;
+    private String medications;
+    private String bloodType;
+    private String insurance;
+
+
+}//Class Patient
---
+++ src/test/java/com/dochart/DochartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dochart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DochartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
