211e96fb1faa62ca77e76f058afed8221942272d
Jamecia Marlynsia <70781425+jameciamarlynsia@users.noreply.github.com>
2020-10-25 18:24:53 -0400

    Add files via upload

---
+++ Inserts for DoChart/prescriptionInserts.sql
@@ -0,0 +1,12 @@
+	INSERT INTO prescription VALUES (02315963,"Zyrtec","Cetirizine","68 to 77F","Johnson & Johnson");
+	INSERT INTO prescription VALUES (00723770,"Xanax","Alprazolam","68 to 77F","Pfizer");
+	INSERT INTO prescription VALUES (02324253,"Vicodin","Hydrocodone/Acetaminophen","68 to 77F","AbbVie Pharmaceuticals");
+	INSERT INTO prescription VALUES (02372959,"Simvastatin","FloLipid","68 to 77F"," Merck"); 
+	INSERT INTO prescription VALUES (02255340,"Azithromycin","generic Zithromax","65 to 70F","Pfizer");
+    INSERT INTO prescription VALUES (02268426,"Lyrica","Pregabalin","65 to 70F","Pfizer");
+    INSERT INTO prescription VALUES (02244521,"Nexium","Esomeprazole","59 to 86F","AstraZeneca");
+     INSERT INTO prescription VALUES ("02322978","Vyvanse","Lisdexamfetamine","68 to 77F","Takeda Pharmaceutical");
+     INSERT INTO prescription VALUES ("00728284","Bumetanide","Bumex","68 to 77F","Pfizer");
+     INSERT INTO prescription VALUES ("00367729","Senna","generic Sennosides","68 to 77F","Sun Pharmaceutical Industries");
+     INSERT INTO prescription VALUES ("02364190","Fentanyl","Durogesic","68 to 77F","Insys Therapeutics Inc");
+	 INSERT INTO prescription VALUES ("02238645","Codeine","Robitussin","68 to 77F","Johnson & Johnson"); 
